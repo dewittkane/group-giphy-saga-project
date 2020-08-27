e0f2f0caeaea62081c432c2f83408bd03ae0d969
Nick Evans <nickevans20@outlook.com>
2020-08-27 11:32:57 -0500

    Setup SearchView and input/button, Need to add functionality to the button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import SearchView from '../SearchView/SearchView';
 
 
 class App extends Component {
 
+  componentDidMount = () => {
+    
+  }
+
   render() {
     return (
+      <>
       <div>
         <h1>Giphy Search!</h1>
       </div>
+      <SearchView />
+
+      </>
     );
   }
   
--- src/index.js
+++ src/index.js
@@ -2,4 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+
+// import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put} from 'redux-saga/effects';
+import axios from 'axios';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const storeInstance = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(sagaMiddleware, logger)
+);
+
+function* watcherSaga() {
+
+}
+
+// starting engine
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('react-root'));
+
+// registerServiceWorker();
