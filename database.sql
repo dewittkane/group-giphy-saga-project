bf524973bb7e028478a79148ccfeea371db4e632
Matt <Kraemermj1992@gmail.com>
2020-08-28 11:33:40 -0500

    added query in get

--- database.sql
+++ database.sql
@@ -11,7 +11,12 @@ CREATE TABLE "favorites" (
     "category_id" VARCHAR (50) REFERENCES "category",
 );
 
-INSERT INTO "favorites" ("image_url") "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmszvtiburon.nl%2Fsecret-santa-steal%2F&psig=AOvVaw0G17f283jyxiyUGeIhp1xr&ust=1598630252323000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMDunfbfu-sCFQAAAAAdAAAAABAO"
+INSERT INTO "favorites" ("image_url") VALUES ('https://www.google.com/url?sa=i&url=https%3A%2F%2Fmszvtiburon.nl%2Fsecret-santa-steal%2F&psig=AOvVaw0G17f283jyxiyUGeIhp1xr&ust=1598630252323000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMDunfbfu-sCFQAAAAAdAAAAABAO');
+INSERT INTO "favorites" ("image_url") VALUES ('https://media.giphy.com/media/h7KvlvWZsfbqjbF9KO/giphy.gif');
+INSERT INTO "favorites" ("image_url") VALUES ('https://media.giphy.com/media/1UNQgDsmZHBApjbTuI/giphy.gif');
+INSERT INTO "favorites" ("image_url") VALUES ('https://media.giphy.com/media/icUEIrjnUuFCWDxFpU/giphy.gif');
+INSERT INTO "favorites" ("image_url") VALUES ('https://media.giphy.com/media/26FmRo4Hmxw3dQrhC/giphy.gif');
+
 SELECT * FROM "favorites" JOIN "category" ON "favorites".category_id = "category".id
 
 
--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,7 +5,15 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
-  res.sendStatus(200);
+  let queryText = `SELECT * FROM "favorites"
+                   JOIN "category" 
+                   ON "favorites".category_id = "category".id`;
+  pool.query(queryText)
+  .then((response) => {res.send(result.rows)})
+  .catch((error) => {
+    console.log('error in get', error);
+    res.sendStatus(500);
+  })
 });
 
 // add a new favorite 
